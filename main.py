f9b80c9c8cf3fd8335d4cc0a31b7685b797050f0
TalosSources <ismail.sahbane@epfl.ch>
2023-12-11 12:20:25 +0100

    main?

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import mc_gurk_classification
 
 model, X, Y, accuracy = mc_gurk_classification.training_pipeline(labels_path='dataset/labels/train_labels.txt',
-                                                                 videos_paths=mc_gurk_classification.sorted_videos_paths('dataset/train_sets/train_bafava'), 
-                                                                 #X_cache_path='dataset/cached_tensors/X_bafava.pt',
+                                                                 #videos_paths=mc_gurk_classification.sorted_videos_paths('dataset/train_sets/train_bagada'), 
+                                                                 X_cache_path='dataset/cached_tensors/X_badaga.pt',
                                                                  epochs=100000, 
                                                                  learning_rate=0.00002,
                                                                  #model_save_path='stuff/saved_models/lr_bafava_a+av.pt', )
